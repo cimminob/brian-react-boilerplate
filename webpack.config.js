3715dfdaa75250480f7af4b3659261a030a824c5
brian desktop <cimminob@gmail.com>
2019-10-28 20:52:07 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+
+    entry: './src/index.js',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Hello Webpack bundled JavaScript Project',
+            template: './src/index.html'
+        })
+    ],
+    devServer: {
+        contentBase: './dist'
+    }
+};
